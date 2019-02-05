b93d62bcd799d35297cad288c19d4b0b7d7423bc
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-05 10:21:08 +0100

    Add files via upload

---
+++ exercice2/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 7</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 2 - Form</h1>
+    
+    <form action="user.php" method="post"> <!-- action est la page vers laquelle on va envoyer les données du formulaire, et ici avec la méthode POST -->
+        <p>Nom</p>
+        <input type="text" name="lastName" />
+        <p>Prénom</p>
+        <input type="text" name="firstName" />
+        <br />
+        <input type="submit" value="Valider" />
+    </form>
+    
+</body>
+</html>
---
+++ exercice2/user.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 7</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 2 - User</h1>
+
+    <p>Bonjour, <?php echo $_POST['lastName'] . ' ' . $_POST['firstName'] ; ?></p>
+
+    <a href="index.php">Retour au formulaire</a>
+
+    
+</body>
+</html>
